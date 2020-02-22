21723a0ecd3568e76d786fe58eeec41c4e4b8fa4
Alex Carvalho Camargo <Alex.Camargo@londonlife.com>
2020-02-22 18:50:09 -0500

    Add i18n

--- wwuatt-ui/vue.config.js
+++ wwuatt-ui/vue.config.js
@@ -2,4 +2,13 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'pt',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 };
